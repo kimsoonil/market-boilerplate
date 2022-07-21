999db098ecd28d37e1e7066e51c426077504aca0
kimsoonil <37436918+kimsoonil@users.noreply.github.com>
2022-07-21 13:15:04 +0900

    Updates

---
+++ precache-manifest.b778e5dd9a95b103d61002f4bf4d2a7e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f322cd9abccce17036b11984a730f7fe",
+    "url": "/market-boilerplate/index.html"
+  },
+  {
+    "revision": "acc4adba5d56fd5fa8dc",
+    "url": "/market-boilerplate/static/css/2.1468693e.chunk.css"
+  },
+  {
+    "revision": "f4a95fb4c1e8a8449b27",
+    "url": "/market-boilerplate/static/css/main.f29ed44e.chunk.css"
+  },
+  {
+    "revision": "acc4adba5d56fd5fa8dc",
+    "url": "/market-boilerplate/static/js/2.1c4e2690.chunk.js"
+  },
+  {
+    "revision": "aa59c44645a420ea7e3b9a6a2f4ff551",
+    "url": "/market-boilerplate/static/js/2.1c4e2690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a95fb4c1e8a8449b27",
+    "url": "/market-boilerplate/static/js/main.cab036e3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/market-boilerplate/static/js/main.cab036e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1495cdf483858229dd47",
+    "url": "/market-boilerplate/static/js/runtime-main.febb8e9f.js"
+  },
+  {
+    "revision": "4148827afc41275424786abcfb3920d2",
+    "url": "/market-boilerplate/static/media/Pretendard-Bold.4148827a.woff"
+  },
+  {
+    "revision": "4b198b1c9c444852fd695a1dede61124",
+    "url": "/market-boilerplate/static/media/Pretendard-Bold.4b198b1c.woff2"
+  },
+  {
+    "revision": "937e6e15d1808255ebb6a76a30ab1fe0",
+    "url": "/market-boilerplate/static/media/Pretendard-ExtraBold.937e6e15.woff"
+  },
+  {
+    "revision": "ab2312a50a9119a3ea951dec27393704",
+    "url": "/market-boilerplate/static/media/Pretendard-ExtraBold.ab2312a5.woff2"
+  },
+  {
+    "revision": "6cb5e4973237865e6a84cda94b02fb88",
+    "url": "/market-boilerplate/static/media/Pretendard-Light.6cb5e497.woff"
+  },
+  {
+    "revision": "c09203bbe3d606b167b2a63794d6c6b5",
+    "url": "/market-boilerplate/static/media/Pretendard-Light.c09203bb.woff2"
+  },
+  {
+    "revision": "343eb4026dc8be42f568c74d847ed4e9",
+    "url": "/market-boilerplate/static/media/Pretendard-Regular.343eb402.woff2"
+  },
+  {
+    "revision": "bcaa3a326de18d845ef56ea101c72211",
+    "url": "/market-boilerplate/static/media/Pretendard-Regular.bcaa3a32.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/market-boilerplate/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/market-boilerplate/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/market-boilerplate/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/market-boilerplate/static/media/slick.f97e3bbf.svg"
+  }
+]);
